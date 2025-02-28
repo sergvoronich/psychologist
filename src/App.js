a1c36981e91b33da627e17bc9eb0abf36568208d
sergvoronich <siarhei.varonich@gmail.com>
2025-02-28 20:49:44 +0300

    few updates

--- src/App.js
+++ src/App.js
@@ -23,11 +23,17 @@ function App() {
       </div>
       <div className='content'>
         <div>
-          <p style={{'margin-bottom' : '0.5rem'}}>Позаботьтесь о своем душевном здоровье</p>
-          <div>Очный прием в г. Гродно или онлайн-консультации</div>
+          <p style={{ 'margin': '2.5rem 0' }}>Позаботьтесь о своем душевном здоровье!</p>
+          <div style={{ 'margin-bottom': '0.5rem' }}>Очный прием в г. Гродно или онлайн-консультации</div>
+          <ul className='content-list'>
+            <li>Дипломированный врач, психолог</li>
+            <li>Собственный опыт борьбы с неврозом</li>
+            <li>Краткосрочная и длительная терапия</li>
+            <li>Безопасно, эффективно и конфиденциально</li>
+          </ul>
         </div>
         <div>
-          <img alt='' height={350} src='/photo_new_2_no_bg.png'/>
+          <img alt='' height={350} src='/photo_new_2_no_bg.png' />
         </div>
       </div>
       <div className='footer'>
